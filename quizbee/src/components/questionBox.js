b98b55451b183dba583f19b019e2f66933f69337
Manaswinich <chinthalapudimanaswini.7@msitprogram.net>
2020-06-27 23:40:54 +0530

    quiz app developed

---
+++ quizbee/src/components/questionBox.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+
+//usgae of hooks is done instead of writing class and using props
+
+const QuestionBox = ({ question, options, selected }) => {
+    const [answer, setAnswer] = useState(options);
+    return (
+        <div className="questionBox">
+            <div classname="question">{question}</div>
+            {answer.map((text, index) => (
+                <button key={index} className="answerBtn"
+                    onClick={() => {
+                        setAnswer([text]);
+                        selected(text);
+                    }}>
+                    {text}
+                </button>
+            ))}
+        </div>
+    );
+};
+
+export default QuestionBox;
